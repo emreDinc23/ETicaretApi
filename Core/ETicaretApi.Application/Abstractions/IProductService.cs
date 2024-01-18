d514bb04c8810e50bb43baed2de547ad9fe23655
Emre Dinç <105745981+emreDinc23@users.noreply.github.com>
2024-01-19 01:41:49 +0300

    Add project files.

---
+++ Core/ETicaretApi.Application/Abstractions/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using ETicaretApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApi.Application.Abstraction
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+
+    }
+}
